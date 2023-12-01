c521342bba65f9c47da5aa50d7abd393390914a0
Daniel <Daniel.Eliasib.AL@outlook.com>
2023-12-01 00:19:31 -0600

    Added colorscheme skeleton code

--- lua/swett-fusion/init.lua
+++ lua/swett-fusion/init.lua
@@ -1,10 +1,26 @@
+local cfg = require('sweet-fusion.config').config
+local palette = require('sweet-fusion.colors').setup()	--TODO: setup(cfg.theme)
+
 local M = {}
 
 M.config = {}
 
+local apply_term_colors = function()
+	-- TODO: Apply like:
+	-- vim.g.terminal_color_0 = palette.black_dark
+end
+
+local apply_theme = function()
+end
 
 function M.load()
+	vim.api.nvim_command('hi clear')	-- TODO: Do I really want to clear all the highlights?
 
+	vim.o.termguicolors = true
+	vim.g.colors_name = 'sweet-fusion'
+
+	apply_term_colors()
+	apply_theme()
 
 end
 
