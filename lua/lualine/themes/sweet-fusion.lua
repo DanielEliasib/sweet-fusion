bb02a41d01d9269f25e182532b7f09b64718da30
Daniel E. Lopez <Danie.Eliasib.AL@outlook.com>
2026-03-05 20:49:35 -0600

    Fix lua line text color

--- lua/lualine/themes/sweet-fusion.lua
+++ lua/lualine/themes/sweet-fusion.lua
@@ -4,41 +4,41 @@ local c = require('sweet-fusion.colors').setup()
 
 sweet_fusion.normal = {
 	a = { bg = c.purple, fg = c.bg_dark, gui = 'bold' },
-	b = { bg = c.grey_dark, fg = c.whitem2 },
-	c = { bg = c.bg_light, fg = c.whitem2 },
+	b = { bg = c.grey_dark, fg = c.white },
+	c = { bg = c.bg_light, fg = c.white },
 }
 
 sweet_fusion.insert = {
 	a = { bg = c.blue, fg = c.bg_dark, gui = 'bold' },
-	b = { bg = c.grey_dark, fg = c.whitem2 },
-	c = { bg = c.bg_light, fg = c.whitem2 },
+	b = { bg = c.grey_dark, fg = c.white },
+	c = { bg = c.bg_light, fg = c.white },
 }
 
 sweet_fusion.command = {
 	a = { bg = c.pink, fg = c.bg, gui = 'bold' },
-	b = { bg = c.grey_dark, fg = c.whitem2 },
-	c = { bg = c.bg_light, fg = c.whitem2 },
+	b = { bg = c.grey_dark, fg = c.white },
+	c = { bg = c.bg_light, fg = c.white },
 }
 
 sweet_fusion.visual = {
 	a = { bg = c.green_var2, fg = c.bg_dark, gui = 'bold' },
-	b = { bg = c.grey_dark, fg = c.whitem2 },
-	c = { bg = c.bg_light, fg = c.whitem2 },
+	b = { bg = c.grey_dark, fg = c.white },
+	c = { bg = c.bg_light, fg = c.white },
 }
 sweet_fusion.replace = {
 	a = { bg = c.red_light, fg = c.bg_dark, gui = 'bold' },
-	b = { bg = c.grey_dark, fg = c.whitem2 },
-	c = { bg = c.bg_light, fg = c.whitem2 },
+	b = { bg = c.grey_dark, fg = c.white },
+	c = { bg = c.bg_light, fg = c.white },
 }
 sweet_fusion.terminal = {
 	a = { bg = c.yellow, fg = c.bg_dark, gui = 'bold' },
-	b = { bg = c.grey_dark, fg = c.whitem2 },
-	c = { bg = c.bg_light, fg = c.whitem2 },
+	b = { bg = c.grey_dark, fg = c.white },
+	c = { bg = c.bg_light, fg = c.white },
 }
 sweet_fusion.inactive = {
 	a = { bg = c.grey_alt, fg = c.bg_dark },
-	b = { bg = c.grey_dark, fg = c.whitem2, gui = 'bold' },
-	c = { bg = c.bg_light, fg = c.whitem2 },
+	b = { bg = c.grey_dark, fg = c.white, gui = 'bold' },
+	c = { bg = c.bg_light, fg = c.white },
 }
 
 return sweet_fusion
